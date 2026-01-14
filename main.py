dee8a036ce23d025504fdf655d91442b130b061c
Yadhu <yadhunandhantj@gmail.com>
2026-01-14 23:33:05 +0530

    included path parameters

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from fastapi import FastAPI # 1. Import FastAPI class
 # Created a FastAPI instance
 app = FastAPI()
 
+
+@app.get("/greet/{name}")
+def greet_user(name: str):
+    return {"message": f"Yo {name}, welcome to the backend!"}
+
 # @ is a decorator
 # Decorator to define a route for HTTP GET requests at the root URL ("/")
 @app.get("/")
 def read_root(): # Function to handle requests to the root URL
     return {"Hello": "the project is alive"} # Return a JSON response for this route
+
